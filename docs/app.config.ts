448f37b9c89f97acd580955dfa4fc7128d4dc57e
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-03-12 18:14:51 -0300

    wip

---
+++ docs/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Nuxt Google Fonts',
+    description: 'Google Fonts for Nuxt.',
+    image: '/cover.jpg',
+    socials: {
+      twitter: 'nuxt_js',
+      github: 'nuxt-community/google-fonts-module'
+    },
+    header: {
+      logo: false
+    }
+  }
+})
--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,26 +1,4 @@
-import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
-  extends: ['./node_modules/@docus/docs-theme'],
-  github: {
-    owner: 'nuxt-community',
-    repo: 'google-fonts-module',
-    branch: 'main'
-  },
-  theme: {},
-  modules: ['@nuxthq/admin', '@docus/github', 'vue-plausible'],
-  plausible: {
-    domain: 'google-fonts.nuxtjs.org'
-  },
-  tailwindcss: {
-    config: {
-      theme: {
-        extend: {
-          colors: {
-            primary: '#1A73E8'
-          }
-        }
-      }
-    }
-  }
+  extends: '@nuxt-themes/docus',
+  modules: ['@nuxtjs/plausible']
 })
